ed11f4bbbf416cc1faf90fb0beaf04e7f30c3e6f
Lady Julia <juliakaykay19@gmail.com>
2023-03-14 16:12:50 +0000

    This is my task for week 2.

---
+++ week_two_task.php
@@ -0,0 +1,17 @@
+<?php
+
+$Age = 18;
+
+$Voter_PVC_Check = "Voter's ward is 020";
+if ($Age = 18 && $Voter_PVC_Check = "Voter's ward is 020") {
+    echo "Voter eligible to vote";
+    
+
+
+} else {
+    echo "You are disqualified from voting. It could be because you are underage, or your voter's ward is not 020";
+
+}
+
+
+    ?>
